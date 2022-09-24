f18078a03161b16de8d819b80cb8e477c6c56d5b
glinux99 <interlab21@yahoo.com>
2022-09-24 12:44:13 +0200

    pdg

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,34 +1,33 @@
-
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="keywords" content="#">
-	<meta name="description" content="#">
-	<title>PDG | @yield('title', 'Acceuil')</title>
-	<!-- Fav and touch icons -->
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="#">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="#">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="#">
-	<link rel="apple-touch-icon-precomposed" href="#">
-	<link rel="shortcut icon" href="{{asset('assets/img/pdg.png')}}">
-	<!-- Bootstrap -->
-	<link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-	<!-- Fontawesome -->
-	<link href="{{asset('assets/css/font-awesome.css')}}" rel="stylesheet">
-	<!-- Flaticons -->
-	<link href="{{asset('assets/css/font/flaticon.css')}}" rel="stylesheet">
-	<!-- Pe-icons -->
-	<link href="{{asset('assets/css/pe-icon-7-stroke.css')}}" rel="stylesheet">
-	<!-- Swiper Slider -->
-	<link href="{{asset('assets/css/swiper.min.css')}}" rel="stylesheet">
-	<!-- Range Slider -->
-	<link href="{{asset('assets/css/ion.rangeSlider.min.css')}}" rel="stylesheet">
-	<!-- magnific popup -->
-	<link href="{{asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
-	<!-- Nice Select -->
-	<link href="{{asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
-	<!-- Custom Stylesheet -->
-	<link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
-	<link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="keywords" content="#">
+    <meta name="description" content="#">
+    <title>PDG | @yield('title', 'Acceuil')</title>
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="#">
+    <link rel="apple-touch-icon-precomposed" href="#">
+    <link rel="shortcut icon" href="{{asset('assets/img/pdg.png')}}">
+    <!-- Bootstrap -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Fontawesome -->
+    <link href="{{asset('assets/css/font-awesome.css')}}" rel="stylesheet">
+    <!-- Flaticons -->
+    <link href="{{asset('assets/css/font/flaticon.css')}}" rel="stylesheet">
+    <!-- Pe-icons -->
+    <link href="{{asset('assets/css/pe-icon-7-stroke.css')}}" rel="stylesheet">
+    <!-- Swiper Slider -->
+    <link href="{{asset('assets/css/swiper.min.css')}}" rel="stylesheet">
+    <!-- Range Slider -->
+    <link href="{{asset('assets/css/ion.rangeSlider.min.css')}}" rel="stylesheet">
+    <!-- magnific popup -->
+    <link href="{{asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
+    <!-- Nice Select -->
+    <link href="{{asset('assets/css/magnific-popup.css')}}" rel="stylesheet">
+    <!-- Custom Stylesheet -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet">
 </head>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,18 +1,47 @@
 <!-- Place all Scripts Here -->
-	<!-- jQuery -->
-	<script src="{{asset('assets/js/jquery.min.js')}}"></script>
-	<!-- Popper -->
-	<script src="{{asset('assets/js/popper.min.js')}}"></script>
-	<!-- Bootstrap -->
-	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-	<!-- Range Slider -->
-	<script src="{{asset('assets/js/ion.rangeSlider.min.js')}}"></script>
-	<!-- Swiper Slider -->
-	<script src="{{asset('assets/js/swiper.min.js')}}"></script>
-	<!-- Nice Select -->
-	<script src="{{asset('assets/js/jquery.nice-select.min.js')}}"></script>
-	<!-- magnific popup -->
-	<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
-	<!-- VITTO Js -->
-	<script src="{{asset('assets/js/main.js')}}"></script>
-	<!-- /Place all Scripts Here -->
+<!-- jQuery -->
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<!-- Popper -->
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<!-- Range Slider -->
+<script src="{{asset('assets/js/ion.rangeSlider.min.js')}}"></script>
+<!-- Swiper Slider -->
+<script src="{{asset('assets/js/swiper.min.js')}}"></script>
+<!-- Nice Select -->
+<script src="{{asset('assets/js/jquery.nice-select.min.js')}}"></script>
+<!-- magnific popup -->
+<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+<!-- VITTO Js -->
+<script src="{{asset('assets/js/main.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script>
+    function sweetAlert() {
+        let timerInterval
+        Swal.fire({
+            confirmButtonColor: '#41CC00',
+            position: 'top-center',
+            className: "bg-danger",
+            icon: "info",
+            html: 'Les informations entrees concernerons votre bureau de vote',
+            timer: 4000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                }, 100)
+            },
+            willClose: () => {
+                clearInterval(timerInterval)
+            }
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('Fermer par le temps')
+            }
+        })
+    }
+</script>
